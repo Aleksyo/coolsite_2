2896c4adb44194baeab939aa97cbdf35f435b736
Trofimov_AP <trofimov_ap@e-sbyt.ru>
2023-03-10 11:50:55 +0900

    lesson 8

--- women/urls.py
+++ women/urls.py
@@ -4,6 +4,10 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
+    path('add_page/', about, name='add_page'),
+    path('contact/', about, name='contact'),
+    path('login/', about, name='login'),
     path('cats/<slug:catid>/', categories),
+    path('post/<int:post_id>/', show_post, name='post'),
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
 ]
--- women/views.py
+++ women/views.py
@@ -2,16 +2,28 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from .models import *
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}]
 
 
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи {post_id}')
 
 
 def categories(request, catid):
